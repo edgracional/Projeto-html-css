79b585282eb916148df7c8893d466c79f1cf52fc
edgracional <edgracional@hotmail.com>
2021-05-24 02:37:01 -0300

    a

--- app.js
+++ app.js
@@ -4,29 +4,47 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var session = require('express-session');
 
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
 
 
 
 
-// Set up mongoose connection
+
+
+const MongoDbStore = require('connect-mongo');
+
+
+
+
 
 var dev_db_url = 'mongodb+srv://edweb:ederse@@edweb.wqrbc.mongodb.net/test';
 var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
 mongoose.Promise = global.Promise;
 var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function(){});
 
 
-
-
-
+app.use(
+    session({
+        secret: 'story book',
+        resave: false,
+        saveUninitialized: false,
+        store: MongoDbStore.create({
+            mongoUrl: 'mongodb+srv://edweb:ederse@@edweb.wqrbc.mongodb.net/test'
+        })
+    })
+);
 
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(express.static(__dirname + '/views'));
+app.use(express.static(__dirname + '/'));
 
 var routes = require('./routes/router');
 app.use('/', routes);
@@ -45,7 +63,6 @@ app.use(function (err, req, res, next) {
 });
 
 
-app.listen(5000, function () {
-  console.log('Listening on port 5000...');
+app.listen(3210, function () {
+  console.log('Listening on port 3210...');
 });
-
